97ba82b49925e7292b5e7d24653d935db1da6969
eunnyoung <12dreamer30@gmail.com>
2022-04-19 03:02:30 +0900

    feat: 마이페이지에 PhotoCardList 적용

--- src/containers/TestPage/MainPage.tsx
+++ src/containers/TestPage/MainPage.tsx
@@ -1,13 +1,14 @@
 import type { NextPage } from 'next';
 import * as S from './MainPage.style';
 import { MainComponents } from './components/MainComponents';
-import { Introduce, MyComments, MyLikes } from 'src/containers/mypage';
+import { Introduce, MyComments, MyLikes, MyPosts } from 'src/containers/mypage';
 import { PhotoCardList } from 'src/components';
 
 export const MainPage: NextPage = () => {
   return (
     <>
       <Introduce />
+      <MyPosts />
       <MyComments />
       <MyLikes />
     </>
--- src/containers/mypage/MyLikes.tsx
+++ src/containers/mypage/MyLikes.tsx
@@ -1,23 +1,58 @@
 import React from 'react';
 import styled from 'styled-components';
 import { TypoGraphy } from '../../components/TypoGraphy';
+import { customColor } from 'src/constants';
+import { PhotoCardList } from 'src/components';
 
 export const MyLikes = () => {
   return (
     <>
-      <div className="myPost">
-        <TypoGraphy>내가 좋아요한 글</TypoGraphy>
-        <TypoGraphy>정보</TypoGraphy>
-        <TypoGraphy>모두의 이야기</TypoGraphy>
-        <hr />
-      </div>
+      <Hr />
+      <Text>
+        <TypoGraphy type="h1" fontWeight="bold">
+          내가 좋아요한 글
+        </TypoGraphy>
+      </Text>
+
+      <Category>
+        <TextCategory>
+          <TypoGraphy
+            type="body1"
+            fontWeight="bold"
+            color={customColor.brownDark}
+          >
+            정보
+          </TypoGraphy>
+        </TextCategory>
+        <TextCategory>
+          <TypoGraphy type="body1" fontWeight="bold" color={customColor.gray}>
+            모두의 이야기
+          </TypoGraphy>
+        </TextCategory>
+      </Category>
+      <PhotoCardList type="MyPage" />
     </>
   );
 };
 
-const Container = styled.div`
+const Hr = styled.hr`
+  margin-top: 80px;
+  width: 1178px;
+  background-color: #c4c4c4;
+`;
+
+const Text = styled.div`
+  padding: 24px;
+`;
+
+const TextCategory = styled.div`
+  padding: 8px;
+  padding-top: 0px;
+  padding-bottom: 32px;
+`;
+const Category = styled.div`
+  position: relative;
   display: flex;
-  flex-direction: column;
   justify-content: center;
   align-items: center;
 `;
