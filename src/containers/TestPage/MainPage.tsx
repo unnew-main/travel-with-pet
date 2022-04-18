abba549c001c281037885c04f19f03a9a4bb9173
eunnyoung <12dreamer30@gmail.com>
2022-04-18 21:59:33 +0900

    Merge branch 'develop' of https://github.com/potato-club/travel-with-pet into feat/MyInfo

--- src/containers/TestPage/MainPage.tsx
+++ src/containers/TestPage/MainPage.tsx
@@ -1,28 +1,15 @@
 import type { NextPage } from 'next';
 import * as S from './MainPage.style';
 import { MainComponents } from './components/MainComponents';
-<<<<<<< HEAD
-import { Introduce, MyComments, MyLikes } from 'src/components/myInfo';
-
-export const MainPage: NextPage = () => {
-  return (
-    // <S.Container>
-    //   <S.Test>hello</S.Test>
-    //   <S.Test2>Test2</S.Test2>
-    //   <Introduce />
-    //   <MainComponents />
-    // </S.Container>
-    <>
-      <Introduce />
-      <MyComments />
-      <MyLikes />
-=======
+import { Introduce, MyComments, MyLikes } from 'src/containers/mypage';
 import { PhotoCardList } from 'src/components';
 
 export const MainPage: NextPage = () => {
   return (
     <>
->>>>>>> 14d6c2efa6aa44f10e2d9a832e971509a6620fa4
+      <Introduce />
+      <MyComments />
+      <MyLikes />
     </>
   );
 };
