7ec1fc672a292f3737fe5d53ce49bca212f70f91
UNNEW-main <1970005826c@gmail.com>
2022-04-18 17:07:46 +0900

    style: Header width 변경

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import type { AppProps } from 'next/app';
 import 'styles/global.css';
 import { LayoutContainer } from 'src/components';
 import { Header } from 'src/components/header/Header';
+import { Footer } from 'src/components/footer/Footer';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <LayoutContainer>
       <Header />
       <Component {...pageProps} />
+      <Footer />
     </LayoutContainer>
   );
 }
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -7,39 +7,37 @@ import Link from 'next/link';
 
 export const Header = () => {
   return (
-    <header>
-      <S.Container>
-        <S.UpHeader>
-          <Link href={'/'} passHref>
-            <S.TitleWrapper>
-              <TypoGraphy
-                type="Title"
-                color={customColor.darkBrown}
-                fontWeight="bold"
-              >
-                MoengMoeng
-              </TypoGraphy>
-            </S.TitleWrapper>
-          </Link>
-          <S.UpLeftWrapper>
-            <S.WriteButtonWrapper>ButtonWrapper</S.WriteButtonWrapper>
-            <S.AlertButtonWrapper>
-              <VscBell />
-            </S.AlertButtonWrapper>
-          </S.UpLeftWrapper>
-        </S.UpHeader>
-        <S.DownHeader>
-          <NavItem pathName={'/information'} customWidth="90">
-            정보
-          </NavItem>
-          <NavItem pathName={'/daily'} customWidth="140">
-            일상
-          </NavItem>
-          <NavItem pathName={'/mypage'} customWidth="100">
-            마이페이지
-          </NavItem>
-        </S.DownHeader>
-      </S.Container>
-    </header>
+    <S.Container>
+      <S.UpHeader>
+        <Link href={'/'} passHref>
+          <S.TitleWrapper>
+            <TypoGraphy
+              type="Title"
+              color={customColor.brownDark}
+              fontWeight="bold"
+            >
+              MoengMoeng
+            </TypoGraphy>
+          </S.TitleWrapper>
+        </Link>
+        <S.UpLeftWrapper>
+          <S.WriteButtonWrapper>ButtonWrapper</S.WriteButtonWrapper>
+          <S.AlertButtonWrapper>
+            <VscBell />
+          </S.AlertButtonWrapper>
+        </S.UpLeftWrapper>
+      </S.UpHeader>
+      <S.DownHeader>
+        <NavItem pathName={'/information'} customWidth="90">
+          정보
+        </NavItem>
+        <NavItem pathName={'/daily'} customWidth="140">
+          일상
+        </NavItem>
+        <NavItem pathName={'/mypage'} customWidth="100">
+          마이페이지
+        </NavItem>
+      </S.DownHeader>
+    </S.Container>
   );
 };
