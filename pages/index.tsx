ae5875f231445ac9f00fe1925a67c5f574e2d5a3
KoJaem <woalswhwh@naver.com>
2022-04-19 16:46:56 +0900

    chore : PR 충돌방지

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,11 @@
 import type { NextPage } from 'next';
-import { PhotoCardTest } from 'src/containers/PhotoCardTest/PhotoCardTest';
 import { MainPage } from '../src/containers';
 
 const Home: NextPage = () => {
   //로직
   return (
     <>
-      {/* <MainPage /> */}
-      <PhotoCardTest/>
+      <MainPage />
     </>
   );
 };
