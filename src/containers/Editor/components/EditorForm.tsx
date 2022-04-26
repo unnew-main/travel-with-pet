123c040a358db62f721885dfff77663fb5dec415
DESKTOP-R375PKT\sanghun <bigyou00@gmail.com>
2022-04-26 21:50:25 +0900

    Memo : 에디터 API 해결법 찾는중

--- src/containers/Editor/components/EditorForm.tsx
+++ src/containers/Editor/components/EditorForm.tsx
@@ -1,12 +1,32 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import { Editor } from '@toast-ui/react-editor';
 import '@toast-ui/editor/dist/toastui-editor.css';
 import 'tui-color-picker/dist/tui-color-picker.css';
 import '@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css';
 import colorSyntax from '@toast-ui/editor-plugin-color-syntax';
+import { useState } from 'react';
 
 function EditorForm(): JSX.Element {
-  const editRef: any = useRef();
+  const editorRef = useRef<Editor>(null);
+
+  const [text, setText] = useState('');
+
+  // useEffect(() => {
+  //   if (editorRef.current) {
+  //     editorRef.current.getInstance().removeHook('addImageBlobHook');
+
+  //     editorRef.current
+  //       .getInstance()
+  //       .addHook('addImageBlobHook', (blob, callback) => {
+  //         const s3config = {
+  //           bucketName: process.env.REACT_APP_BUCKET_NAME as string,
+  //           region: process.env.REACT_APP_REGION as string,
+  //           accessKeyId: process.env.REACT_APP_ACCESS_ID as string,
+  //           secretAccessKey: process.env.REACT_APP_ACCESS_KEY as string,
+  //         };
+  //       });
+  //   }
+  // }, []);
 
   return (
     <Editor
@@ -16,7 +36,7 @@ function EditorForm(): JSX.Element {
       initialEditType="markdown"
       useCommandShortcut={true}
       plugins={[colorSyntax]}
-      ref={editRef}
+      ref={editorRef}
     />
   );
 }
