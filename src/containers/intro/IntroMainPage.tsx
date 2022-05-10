cb1fb50f6f97003a413e8363e137b9c08a124292
DESKTOP-R375PKT\sanghun <bigyou00@gmail.com>
2022-05-10 22:00:31 +0900

    Feat : 인터렉션 추가함

--- src/containers/intro/IntroMainPage.tsx
+++ src/containers/intro/IntroMainPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { TypoGraphy, Seo } from 'src/components';
 import { customColor } from 'src/constants/customColor';
 import styled from 'styled-components';
@@ -7,10 +7,15 @@ import pet1 from 'src/assets/image/pet1.jpg';
 import pet2 from 'src/assets/image/pet2.jpg';
 import pet3 from 'src/assets/image/pet3.jpg';
 import pet4 from 'src/assets/image/pet4.jpg';
-
 import { ImArrowRight2 } from 'react-icons/im';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 export const IntroMainPage = () => {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
   return (
     <Wrapper>
       <TitleContent>
@@ -21,53 +26,82 @@ export const IntroMainPage = () => {
           <Title>meongmeong</Title>
         </TitleBox>
       </TitleContent>
+
       <Content>
         <ImageWrapper>
-          <Image src={pet1} alt="강아지사진1" width="600" height="600" />
+          <div data-aos="fade-right" data-aos-easing="ease-in-sine">
+            <Image src={pet1} alt="강아지사진1" width="600" height="600" />
+          </div>
         </ImageWrapper>
         <TextWrapper>
-          <TypoGraphy type="body1" fontWeight="bold">
+          <TypoGraphy type="h3" fontWeight="bold">
             지루해하는 강아지를 위해 여행을 가고 싶거나
           </TypoGraphy>
         </TextWrapper>
       </Content>
+
       <Content>
         <TextWrapper>
-          <TypoGraphy type="body1" fontWeight="bold">
+          <TypoGraphy type="h3" fontWeight="bold">
             색다른 곳에서 산책하고
           </TypoGraphy>
         </TextWrapper>
         <ImageWrapper>
-          <Image src={pet2} alt="강아지사진1" width="600" height="600" />
+          <div data-aos="fade-left" data-aos-easing="ease-in-sine">
+            <Image src={pet2} alt="강아지사진2" width="600" height="600" />
+          </div>
         </ImageWrapper>
       </Content>
       <Content>
         <ImageWrapper>
-          <Image src={pet3} alt="강아지사진1" width="600" height="600" />
+          <Image
+            data-aos="fade-right"
+            data-aos-easing="ease-in-sine"
+            src={pet3}
+            alt="강아지사진3"
+            width="600"
+            height="600"
+          />
         </ImageWrapper>
         <TextWrapper>
-          <TypoGraphy type="body1" fontWeight="bold">
+          <TypoGraphy type="h3" fontWeight="bold">
             강아지와 함께 앉아 맛있는 식사를 하고
           </TypoGraphy>
         </TextWrapper>
       </Content>
       <Content>
         <TextWrapper>
-          <TypoGraphy type="body1" fontWeight="bold">
+          <TypoGraphy type="h3" fontWeight="bold">
             강아지에 관련된 다양한 정보를 공유하고 싶으시다면
           </TypoGraphy>
         </TextWrapper>
         <ImageWrapper>
-          <Image src={pet4} alt="강아지사진1" width="600" height="600" />
+          <Image
+            data-aos="fade-left"
+            data-aos-easing="ease-in-sine"
+            src={pet4}
+            alt="강아지사진4"
+            width="600"
+            height="600"
+          />
         </ImageWrapper>
       </Content>
-      <TypoGraphy fontWeight="bold" type="Title">
-        이곳으로 오세요
-      </TypoGraphy>
-      <Title>meongmeong</Title>
-      <TypoGraphy>
-        <ArrowBtn />
-      </TypoGraphy>
+      <BottomContent>
+        <TypoGraphy fontWeight="bold" type="Title">
+          이곳으로 오세요
+        </TypoGraphy>
+        <Title>meongmeong</Title>
+        <TypoGraphy>
+          <AnimationArrow
+            data-aos="fade-right"
+            data-aos-offset="-500"
+            data-aos-easing="ease-in-sine"
+            data-aos-anchor-placement="bottom-center"
+          >
+            <ArrowBtn onClick={() => alert('로그인 로직')} />
+          </AnimationArrow>
+        </TypoGraphy>
+      </BottomContent>
     </Wrapper>
   );
 };
@@ -111,9 +145,9 @@ const TitleBox = styled.div`
 
 const Title = styled.div`
   color: white;
-  text-shadow: 0px 0px 8px ${customColor.brownDark};
-  text-shadow: -1px 0 ${customColor.brownDark}, 0 1px ${customColor.brownDark},
-    1px 0 ${customColor.brownDark}, 0 -1px ${customColor.brownDark};
+  text-shadow: 1px 0 4px ${customColor.brownDark},
+    0 1px 4px ${customColor.brownDark}, -1px 0 4px ${customColor.brownDark},
+    0 -1px 4px ${customColor.brownDark};
   font-size: 48px;
   font-weight: bold;
 `;
@@ -121,4 +155,19 @@ const Title = styled.div`
 const ArrowBtn = styled(ImArrowRight2)`
   font-size: 36px;
   margin: 120px 0 100px 0;
+`;
+
+const BottomContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 1200px;
+  margin: 200px 0;
+`;
+
+const AnimationArrow = styled.div`
+  cursor: pointer;
+  &:hover {
+    color: ${customColor.gray};
+  }
 `;
