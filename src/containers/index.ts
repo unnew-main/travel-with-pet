2a8736f80d20a32f0d6fce18410e8dc83a662d09
KoJaem <woalswhwh@naver.com>
2022-05-12 01:19:52 +0900

    style : 반응형 구현 및 파일구조 변경

--- src/containers/index.ts
+++ src/containers/index.ts
@@ -2,3 +2,4 @@ export { EditorMainPage } from './Editor/EditorMainPage';
 export { IntroMainPage } from './intro/IntroMainPage';
 export { InformationPage } from './InformationPage/InformationPage';
 export { DailyPage } from './DailyPage/DailyPage';
+export { MoreViewPage } from './moreViewPage/MoreViewPage';
